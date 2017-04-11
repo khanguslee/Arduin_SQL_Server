ff6ccaef2f98e65fe7a23cd5969f620b882bbe40
Angus Lee <angus.lee809@gmail.com>
2017-04-12 08:53:12 +1000

    Add files via upload
    - Functional code

--- arduino_test/add_data.php
+++ arduino_test/add_data.php
@@ -1,12 +1,12 @@
 <?php 
 	// Connect to MySQL
-	include("dbconnect.php");
+	include("\dbconnect.php");
 	
 	//Prepare the SQL statement
 	$SQL = "INSERT INTO machinelogdb.machine_time(machine, time_start, time_end, length_time) VALUES ('".$_GET["machine"]."', '".$_GET["start"]."', '".$_GET["end"]."', '".$_GET["length"]."')";
 
 	// Execute SQL statement
-	mysql_query($SQL);
+	mysqli_query($dbh, $SQL);
 	
 	// Go to the review_data.php
 	header("Location: review_data.php");
